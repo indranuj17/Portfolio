5e4d83e74985666a76d227c35358706de681986b
indranuj17 <indranujdev015@gmail.com>
2024-07-13 21:58:59 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Hero from '../assets/Hero.png'
 import { HiChevronDoubleRight } from "react-icons/hi";
+import {Link} from 'react-scroll';
+
 
 const Home = () => {
   return (
@@ -10,7 +12,7 @@ const Home = () => {
           <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Frontend Developer</h2>
           <p className=' py-4 text-gray-400 max-w-md'>I'm a frontend developer with a keen eye for detail, creating seamless and engaging user experiences. Skilled in HTML, CSS, JavaScript, and modern frameworks like  Tailwind(CSS) and React, I bring designs to life.</p>
         <div >
-        <button className='group flex items-center max-w-fit rounded-md px-6 py-3 mt-3 text-white bg-gradient-to-r from-cyan-400 to-indigo-600  cursor-pointer font-semibold'>Portfolio
+        <button className='group flex items-center max-w-fit rounded-md px-6 py-3 mt-3 text-white bg-gradient-to-r from-cyan-400 to-indigo-600  cursor-pointer font-semibold'><Link to="About" spy={true} smooth={true}offset={50}duration={500}>Portfolio</Link>
           <span className='ml-3 group-hover:rotate-90 duration-200'><HiChevronDoubleRight size={18} />
           </span>
         </button>
